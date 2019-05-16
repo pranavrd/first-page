09312ea9d92c6ff7dfb17957000d41647e3f8fa0
Pranav Doshetty <pranav.doshetty@gmail.com>
2019-05-17 01:06:26 +0530

    supercharge #1

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,31 @@
-var myHeading = document.querySelector('h2');
-myHeading.textContent = 'I knew it!';
+var myImage=document.querySelector('img');
+
+myImage.onclick=function(){
+	var mySrc=myImage.getAttribute('src');
+	if(mySrc === 'images/Scarlett.jpg'){
+		myImage.setAttribute('src','images/Chris.jpg');
+	}
+	else{
+		myImage.setAttribute('src','images/Scarlett.jpg');
+	}
+}
+
+var myButton=document.querySelector('button');
+var myHeading=document.querySelector('h2');
+
+function setUserName() {
+	var myName=prompt('Please enter your name.');
+	localStorage.setItem('name',myName);
+	myHeading.textContent='Told you so, '+myName+'!';
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Told you so, ' + storedName +'!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
